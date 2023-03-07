0f3e8b16c9416fb1ed5086d5a0c3f6f72f68e7f2
Josh Oh <joshuayoh@gmail.com>
2023-03-07 17:48:57 -0600

    Update peter_test.py
    updating actual script (forgot the first time)

--- peter_test.py
+++ peter_test.py
@@ -1,6 +1,6 @@
+import tkinter as tk
 import RPi.GPIO as GPIO
 from time import sleep
-import keyboard as kb
 
 # Direction pin from controller
 DIR = 10
@@ -19,24 +19,30 @@ GPIO.setmode(GPIO.BOARD)
 GPIO.setup(DIR, GPIO.OUT)
 GPIO.setup(STEP, GPIO.OUT)
 
-try:
-	# Run forever.
-	while True:
-		while kb.is_pressed('u'):
-			GPIO.output(DIR,CW)
-			GPIO.output(STEP, GPIO.HIGH)
-			sleep(.005)
-			GPIO.output(STEP,GPIO.LOW)
-			sleep(.005)
-		while kb.is_pressed('d'):
-			GPIO.output(DIR,CCW)
-			GPIO.output(STEP, GPIO.HIGH)
-			sleep(.005)
-			GPIO.output(STEP,GPIO.LOW)
-			sleep(.005)
+def move_cw():
+    GPIO.output(DIR, CW)
+    GPIO.output(STEP, GPIO.HIGH)
+    sleep(.005)
+    GPIO.output(STEP, GPIO.LOW)
+    sleep(.005)
+
+def move_ccw():
+    GPIO.output(DIR, CCW)
+    GPIO.output(STEP, GPIO.HIGH)
+    sleep(.005)
+    GPIO.output(STEP, GPIO.LOW)
+    sleep(.005)
+
+root = tk.Tk()
+root.title("Stepper Motor Control")
+
+button_cw = tk.Button(root, text="Clockwise", command=move_cw)
+button_cw.pack()
+
+button_ccw = tk.Button(root, text="Counter-Clockwise", command=move_ccw)
+button_ccw.pack()
+
+root.mainloop()
 
 # Once finished clean everything up
-
-except KeyboardInterrupt:
-	print("cleanup")
-	GPIO.cleanup()
+GPIO.cleanup()
